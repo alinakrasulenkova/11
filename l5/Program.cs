217fcf525b3cb9a89cd1d387ae0fe66e0f4de989
Alina <ajweis777@yandex.ru>
2022-11-28 14:27:10 +0300

    dz2

---
+++ l5/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {-8,5,4,6,7,-2};
+int [] array2 =new int[array.Length/2+array.Length%2];
+
+int index =array.Length -1;
+
+
+for (int i = 0; i < array2.Length; i++)
+{
+array2[i]= array[i]*array[index-i];
+
+if (i==index-i)
+{
+array2[i]= array[i];
+}
+}
+
+Console.WriteLine(String.Join(" ", array2));
