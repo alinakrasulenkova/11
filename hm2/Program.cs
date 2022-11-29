9e9431512ec02ba96e00c82bc1404fbc7887a522
Alina <ajweis777@yandex.ru>
2022-11-29 11:12:08 +0300

    dz2

--- hm2/Program.cs
+++ hm2/Program.cs
@@ -4,15 +4,24 @@
 
 // [345, 897, 568, 234] -> 2
 
-int [] array = {154,865,478,621,756, 893, 552};
 
-int count=0;
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
 
-for (int i = 0; i < array.Length; i++)
-
- if (i % 2 == 0)
+int [] GetArray(int[] numbers)
+{
+for (int i = 0; i < numbers.Length; i++)
+{
+numbers[i]= new Random().Next(99,1000);   
+    if (numbers [i] % 2 == 0)
     {
         count++;
     }
-
-    Console.WriteLine(count);
+}
+return numbers;
+}
+GetArray(numbers);
+Console.WriteLine(String.Join(" ",numbers));
+Console.WriteLine(count);
---
+++ hm3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу
+//  между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int[size];
+int dif = 0;
+int min = 0;
+int max = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= new Random().Next(-100,100);   
+    
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+      
+}
+
+dif = max - min;
+Console.WriteLine(String.Join(" ",numbers));
+Console.WriteLine("Максимальное " +max);
+Console.WriteLine("Минимальное " +min);
+Console.WriteLine("разница " +dif);
